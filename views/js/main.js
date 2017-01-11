f6d69f26878b24a7c5989f8cadfbef8e2dc6c9e2
vijpandaturtle <thisisvij98@gmail.com>
2017-01-12 01:01:15 +0530

    Code modified

--- views/js/main.js
+++ views/js/main.js
@@ -360,17 +360,11 @@ var makeRandomPizza = function() {
 
 // returns a DOM element for each pizza
 var pizzaElementGenerator = function(i) {
-  var pizzaContainer,             // contains pizza title, image and list of ingredients
-      pizzaImageContainer,        // contains the pizza image
-      pizzaImage,                 // the pizza image itself
-      pizzaDescriptionContainer,  // contains the pizza title and list of ingredients
-      pizzaName,                  // the pizza name itself
-      ul;                         // the list of ingredients
 
-  pizzaContainer = document.createElement("div");
-  pizzaImageContainer = document.createElement("div");
-  pizzaImage = document.createElement("img");
-  pizzaDescriptionContainer = document.createElement("div");
+ var pizzaContainer = document.createElement("div");              // contains pizza title, image and list of ingredients
+ var pizzaImageContainer = document.createElement("div");        // contains the pizza image
+ var pizzaImage = document.createElement("img");                // the pizza image itself
+ var pizzaDescriptionContainer = document.createElement("div"); // contains the pizza title and list of ingredients
 
   pizzaContainer.classList.add("randomPizzaContainer");
   pizzaContainer.style.width = "33.33%";
@@ -386,11 +380,11 @@ var pizzaElementGenerator = function(i) {
 
   pizzaDescriptionContainer.style.width="65%";
 
-  pizzaName = document.createElement("h4");
+  var pizzaName = document.createElement("h4"); // the pizza name itself
   pizzaName.innerHTML = randomName();
   pizzaDescriptionContainer.appendChild(pizzaName);
 
-  ul = document.createElement("ul");
+  var ul = document.createElement("ul"); // the list of ingredients
   ul.innerHTML = makeRandomPizza();
   pizzaDescriptionContainer.appendChild(ul);
   pizzaContainer.appendChild(pizzaDescriptionContainer);
