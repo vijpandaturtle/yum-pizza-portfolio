be76094c99ecea85842006fe88a0a2fd121038ee
vijpandaturtle <thisisvij98@gmail.com>
2017-01-14 15:40:06 +0530

    code modified

--- views/js/main.js
+++ views/js/main.js
@@ -494,8 +494,8 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
   var items = document.querySelectorAll('.mover');
-  var scroll = document.body.scrollTop
   for (var i = 0; i < items.length; i++) {
+      var scroll = document.body.scrollTop
       var phase = Math.sin((scroll/1250) + (i % 5));
       console.log(phase, scroll/1250);
      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
